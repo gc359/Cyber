9b2fdd4da3a0f20e11ab8ffdc222dd8d58d27622
unknown <gc359@njit.edu>
2023-05-06 22:27:07 -0400

    done creating network_scanner

---
+++ Beginner/Network Scanner/network_scanner.py
@@ -0,0 +1,23 @@
+
+import subprocess
+ 
+ 
+'''
+ICMP Echo Request
+It is also known by using ‘ping command’. An ICMP packet is sent to a host using the IP address and if the ICMP echo is received, that means that the host is online and is receiving the signals. For this, it necessary to get all the IP addresses for which you wish to test that the host is connected or not. This method works on the assumption that network devices have ICMP enabled.
+''' 
+
+for ping in range(1,10):
+    address = "127.0.0." + str(ping)
+    res = subprocess.call(['ping', '-c', '3', address])
+    if res == 0:
+        print( "ping to", address, "OK")
+    elif res == 2:
+        print("no response from", address)
+    else:
+        print("ping to", address, "failed!")
+        
+'''
+This code iterates over all the available IP addresses, ping them and check for the reply. If the echo is received, that means the host is connected and in case, no echo is received, then it looks like that the host is down.
+Note: Personal firewalls or general firewalls are often set to so called “stealth mode” which is used not to react to ICMP echo requests.
+'''
---
+++ Beginner/Network Scanner/readme.md
@@ -0,0 +1,3 @@
+-A network scanner is one major tool for analyzing the hosts that are available on the network. 
+-A network scanner is an IP scanner that is used for scanning the networks that are connected to several computers.
+
